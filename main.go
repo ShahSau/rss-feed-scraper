dbb102725f45948132014c07b36fae0ec02d2a80
shahriar <shahriarksaurov@gmail.com>
2024-07-13 14:09:35 +0200

    boilerplate

--- main.go
+++ main.go
@@ -3,17 +3,49 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
 	godotenv.Load(".env")
 	port := os.Getenv(("PORT"))
 	if port == "" {
 		log.Fatal("$PORT must be set")
 	}
 	fmt.Println("Server is running on port: ", port)
+
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		MaxAge:           300,
+		AllowCredentials: false,
+		ExposedHeaders:   []string{"Link"},
+	}))
+
+	v1Router := chi.NewRouter()
+	v1Router.Get("/health", handlerReadiness)
+	v1Router.Get("/error", handlerError)
+
+	router.Mount("/v1", v1Router) // Mount the v1Router under /v1
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + port,
+	}
+
+	log.Printf("Server is running on port: %s", port)
+	err := srv.ListenAndServe()
+
+	if err != nil {
+		log.Fatal("Server failed to start: ", err)
+	}
+
 }
